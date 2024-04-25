42ab86c3dedd9e2de24ed0d33bd6dc6b8001506e
Samuel de Carvalho <carvalhos.samuel86@gmail.com>
2024-04-25 18:05:37 -0300

    Finalizando projeto

--- src/routes/person.route.js
+++ src/routes/person.route.js
@@ -31,4 +31,40 @@ router.get('/list', async(req, res)=>{
     }
 })
 
+router.get('/edit/:id', async(req, res)=>{
+    try{
+    const {id} = req.params;
+    const [person] = await pool.query('SELECT * FROM person WHERE id = ?', [id]);
+    const personEdit = person[0];
+    res.render('person/edit', {person: personEdit});
+}
+catch(err){
+    res.status(500).json({message:err.message});
+}
+})
+
+router.post('/edit/:id', async(req, res)=>{
+    try{
+        const {name, lastname, age} = req.body;
+        const {id} = req.params;
+        const editPerson = {name, lastname, age};
+        await pool.query('UPDATE person SET ? WHERE id = ?', [editPerson, id]);
+        res.redirect('/list');
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+})
+
+router.get('/delete/:id', async(req, res)=>{
+    try{
+        const {id} = req.params;
+        await pool.query('DELETE FROM person WHERE id = ?', [id]);
+        res.redirect('/list');
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+})
+
 export default router;
